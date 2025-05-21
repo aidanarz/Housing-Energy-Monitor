b08ccb71809aa01385f18737cc0b56e3a5da58ac
aidanarz <aidan.ardhazizi@gmail.com>
2025-05-21 11:11:05 +0700

    add input perangkat dan list batas normal

--- main.c
+++ main.c
@@ -1,5 +1,37 @@
-# include <stdio.h>
+#include <stdio.h>
+#include <string.h>
+
+const char* namaAlat[] = {"AC", "KULKAS", "MESIN CUCI", "SETRIKA", "DISPENSER", "POMPA AIR", "TV", "LAMPU", "RICE COOKER"};
+
+typedef struct {
+    int alats;
+    int jamPemakaian;    
+} alat;
+
 
 int main() {
+    alat alat1[9];
+
+    for(int i = 0; i < 9; i++) {
+        printf("input jumlah perangkat %s dan berapa jam digunakan dalam sehari\n", namaAlat[i]);
+        scanf("%d %d", &alat1[i].alats, &alat1[i].jamPemakaian);
+    }
+
+    for (int i = 0; i < 9; i++) {
+        printf("\nSetiap %d %s menyala selama %d jam/hari", alat1[i].alats, namaAlat[i], alat1[i].jamPemakaian);
+    }
     
+    // list batas normal
+    // AC = 84 kWh / 30 hari, 4jam/hari
+    // KULKAS = 108 kWh / 30 hari, 24jam/hari
+    // MESIN CUCI = 6 kWh / 30 hari, 2jam/hari
+    // SETRIKA = 5 kWh / 30 hari, 1jam/hari
+    // DISPENSER = 30 kWh / 30 hari, 24jam/hari
+    // POMPA AIR = 15 kWh / 30 hari, 1jam/hari
+    // TV = 18 kWh / 30 hari, 5jam/hari
+    // LAMPU(10) = 30 kWh / 6 hari, 18jam/hari
+    // RICE COOKER = 24 kWh / 30 hari, 2jam/hari
+
+    
+    return 0;
 }
