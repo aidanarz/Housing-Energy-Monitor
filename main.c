9e55a530848b44881278684dba715af82d5fb74e
rajavijasa <jarotvijasa@gmail.com>
2025-05-22 11:55:22 +0700

    Finalisasi 50%

--- main.c
+++ main.c
@@ -19,6 +19,13 @@ typedef enum{
     OVER
 }statusAlat;
 
+typedef enum {
+    KOS,
+    KECIL,
+    SEDANG,
+    BESAR
+}tipeRumah; 
+
 typedef struct {
     int jumlahAlat;
     int jamPemakaian;    
@@ -26,28 +33,99 @@ typedef struct {
     int jumlahKonsumsi;
 } alat;
 
-typedef enum {
-    KECIL,
-    SEDANG,
-    BESAR
-}tipeRumah; 
+typedef struct {
+    float total;
+    tipeRumah tpRumah;
+}info;
+
+void tipe_rumah(alat *alat_point, info *infoRumah);
+void total_konsumsi(alat *alat_point);
+void perbandingan(alat *alat_point);
+void rekomendasi(alat *alat_point);
+
+int main() {
+    alat alat1[9];
+    info infoRumah;
+
+    for(int i = 0; i < 9; i++) {
+        printf("input jumlah perangkat %s dan berapa jam digunakan dalam sehari\n", namaAlat[i]);
+        scanf("%d %d", &alat1[i].jumlahAlat, &alat1[i].jamPemakaian);
+        if(alat1[i].jamPemakaian > 24 || alat1[i].jumlahAlat < 0 || alat1[i].jamPemakaian < 0) {
+            printf("Jumlah perangkat atau jam penggunaannya salah!\n");
+            i--;
+        }
+    }
+
+    total_konsumsi(alat1);
+
+    for (int i = 0; i < 9; i++) {
+        printf("%d %s menyala selama %d jam/hari dengan masing masing memakan daya: %d Wh\n", alat1[i].jumlahAlat, namaAlat[i], alat1[i].jamPemakaian, (alat1[i].jumlahKonsumsi/30));
+    }
+
+    tipe_rumah(alat1, &infoRumah);
+    perbandingan(alat1);
+    printf("\nTotal Konsumsi Listrik Selama Sebulan %.2f KWh", (infoRumah.total / KILO));
+    rekomendasi(alat1);
+
+    puts("");
+
+    switch (infoRumah.tpRumah) {
+        case KOS:
+            puts("Anda tinggal di KOS!");
+            break;
+        case KECIL:
+            puts("Tipe rumah anda KECIL!");
+            break;
+        case SEDANG:
+            puts("Tipe rumah anda SEDANG!");
+            break;
+        case BESAR:
+            puts("Tipe rumah anda BESAR!");
+            break;
+    }
+    
+    return 0;
+}
+
+void tipe_rumah(alat *alat_point, info *infoRumah) {
+    infoRumah->total = 0;
+
+    for(int i = 0; i < 9; i++) {
+        infoRumah->total += alat_point[i].jumlahKonsumsi;
+    } 
+    
+    if(infoRumah->total >= 50 * KILO && infoRumah->total <= 150 * KILO) {
+        infoRumah->tpRumah = KECIL;
+    }
+    else if(infoRumah->total > 150 * KILO && infoRumah->total <= 300 * KILO) {
+        infoRumah->tpRumah = SEDANG;
+    }
+    else if(infoRumah->total > 300 * KILO) {
+        infoRumah->tpRumah = BESAR;
+    }
+    else {
+        infoRumah->tpRumah = KOS;
+    }
+}
 
 void total_konsumsi(alat *alat_point) {
+
     int daya[] = {
         WATT_AC, WATT_KULKAS, WATT_MESIN_CUCI, WATT_SETRIKA,
         WATT_DISPENSER, WATT_POMPA_AIR, WATT_TV, WATT_LAMPU, WATT_RICE_COOKER
     };
 
     for(int i = 0; i < 9; i++) {
-        alat_point[i].jumlahKonsumsi = daya[i] * alat_point[i].jamPemakaian * alat_point[i].jumlahAlat;
-    }
+        alat_point[i].jumlahKonsumsi = (daya[i] * alat_point[i].jamPemakaian * alat_point[i].jumlahAlat * 30);        
+    }    
 }
 
 void perbandingan(alat *alat_point) {
     int batas[] = {
-        84 * KILO, 108 * KILO, 6 * KILO, 5 * KILO,
-        30 * KILO, 15 * KILO, 18 * KILO, 30 * KILO, 24 * KILO
+    84 * KILO, 108 * KILO, 25 * KILO, 25 * KILO,
+    45 * KILO, 30 * KILO, 20 * KILO, 2 * KILO, 35 * KILO
     };
+
 
     for (int i = 0; i < 9; i++) {
         if (alat_point[i].jumlahKonsumsi > batas[i]) {
@@ -58,7 +136,7 @@ void perbandingan(alat *alat_point) {
     }
 }
 
-void rekomendasi(alat *alat_point){
+void rekomendasi(alat *alat_point) {
     printf("\n\nRekomendasi: \n");
     for(int i = 0; i < 9; i++){
         switch(i){
@@ -74,7 +152,7 @@ void rekomendasi(alat *alat_point){
             case 1:
                 printf("\nKULKAS: \n");
                 if(alat_point[i].status == OVER){
-                    printf("- Cek karet pintu & freon\n- Jangan buka pint1u terlalu lama\n- Jaga jarak kulkas dari dinding\n");
+                    printf("- Cek karet pintu & freon\n- Jangan buka pintu terlalu lama\n- Jaga jarak kulkas dari dinding\n");
                 }
                 else{
                     printf("Sudah digunakan secara efisien\n");
@@ -128,7 +206,7 @@ void rekomendasi(alat *alat_point){
             case 7:
                 printf("\nLampu: \n");
                 if(alat_point[i].status == OVER){
-                    printf("LAMPU: \n- Gunakan lampu LED\n- Matikan saat tidak digunakan\n");
+                    printf("- Gunakan lampu LED\n- Matikan saat tidak digunakan\n");
                 }
                 else{
                     printf("Sudah digunakan secara efisien\n");
@@ -145,56 +223,4 @@ void rekomendasi(alat *alat_point){
                 break;
         }
     }
-}
-
-int main() {
-    alat alat1[9];
-
-    for(int i = 0; i < 9; i++) {
-        printf("input jumlah perangkat %s dan berapa jam digunakan dalam sehari\n", namaAlat[i]);
-        scanf("%d %d", &alat1[i].jumlahAlat, &alat1[i].jamPemakaian);
-        if(alat1[i].jamPemakaian > 24 || alat1[i].jumlahAlat < 1 || alat1[i].jamPemakaian < 1) {
-            printf("Jumlah perangkat atau jam penggunaannya salah!\n");
-            i--;
-        }
-    }
-
-    for (int i = 0; i < 9; i++) {
-        printf("\nSetiap %d %s menyala selama %d jam/hari", alat1[i].jumlahAlat, namaAlat[i], alat1[i].jamPemakaian);
-    }
-
-    total_konsumsi(alat1);
-    perbandingan(alat1);
-    rekomendasi(alat1);
-    
-    // list batas normal
-    // AC = 84 kWh / 30 hari, 4jam/hari
-    // KULKAS = 108 kWh / 30 hari, 24jam/hari
-    // MESIN CUCI = 6 kWh / 30 hari, 2jam/hari
-    // SETRIKA = 5 kWh / 30 hari, 1jam/hari
-    // DISPENSER = 30 kWh / 30 hari, 24jam/hari
-    // POMPA AIR = 15 kWh / 30 hari, 1jam/hari
-    // TV = 18 kWh / 30 hari, 5jam/hari
-    // LAMPU(10) = 30 kWh / 6 hari, 18jam/hari
-    // RICE COOKER = 24 kWh / 30 hari, 2jam/hari
-
-    // list daya
-    // AC = 500 watt
-    // KULKAS = 150 watt
-    // MESIN CUCI = 500 watt
-    // SETRIKA = 300 watt
-    // DISPENSER = 150 watt
-    // POMPA AIR = 500 watt
-    // TV = 120 watt
-    // LAMPU = 10 watt
-    // RICE COOKER = 400 watt
-
-    // fungsi hitung total konsumsi
-    // fungsi nentuin tipe rumah pake enum
-    // fungsi perbandingan batas normal
-    // fungsi ngasih tau rekomendasi efisiensi daya
-    // Tes jarararararod
-    // ketwa hahaha 
-
-    return 0;
 }
